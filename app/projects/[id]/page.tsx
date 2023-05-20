87fe1ec4beaf5e36f6451f3d06fa219e34d3c370
teun <teun@d-centralize.nl>
2023-05-20 11:03:05 +0200

    feat(project): multiple images

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,16 +1,41 @@
 import { projects } from "@/data/projects";
-import Project from "@/components/project";
+import NavButton from "@/components/navButton";
+import Image from "next/image";
 
 export default function ProjectPage({ params }: { params: any }) {
   const project = projects.find((project) => project.id === params.id);
 
   if (!project) return <div>404 page not found</div>;
 
+  const title = project.title;
+  const description = project.description;
+  const images = project.images;
+
   return (
-    <Project
-      title={project.title}
-      description={project.description}
-      image={project.image}
-    />
+    <div className="h-screen px-4 md:px-20 lg:px-40 pt-10 md:pt-0">
+      <NavButton text="👈Projects" href="/projects" />
+      <div
+        className={
+          "flex flex-col gap-4 items-center justify-start pt-4 md:px-20 lg:px-30 xl:px-40 md:pt-0 md:justify-center h-full font-bold text-6xl"
+        }
+      >
+        <h3 className="font-semibold font-sans text-3xl">{project.title}</h3>
+        <p className={"text-xl"}>{project.description}</p>
+        {images && (
+          <div className="flex gap-4 flex-row items-start flex-wrap w-full justify-center">
+            {images.map((image) => (
+              <Image
+                className="object-contain text-md"
+                key={image}
+                src={image}
+                alt="project image"
+                width={500}
+                height={1000}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,31 +5,33 @@ import NavButton from "@/components/navButton";
 export default function NewProjectsPage() {
   return (
     <>
-      <div className="flex flex-col lg:flex-row justify-center gap-10 h-screen">
-        <NavButton text="🏠Home" href="/" />
-        <ProjectColumn title="Semester 1">
-          <ProjectLink
-            name="Euro-Dollar converter"
-            href="/projects/eurodollarconverter"
-          />
-          <ProjectLink
-            name="Shop Register System"
-            href="/projects/shop-register-system"
-          />
-        </ProjectColumn>
-        <ProjectColumn title="Semester 2">
-          <ProjectLink name="Circus Train" href="/projects/circustrain" />
-          <ProjectLink
-            name="Bolus Calculator"
-            href="/projects/bolus-calculator"
-          />
-          <ProjectLink name="Dream Journal" href="/projects/dream-journal" />
-          <ProjectLink name="StudyCluster" href="/projects/studycluster" />
-        </ProjectColumn>
-        <ProjectColumn title="Semester 3">
-          <ProjectLink name="Ninja Game" href="/projects/ninja-game" />
-          <ProjectLink name="Bimpi" href="/projects/bimpi" />
-        </ProjectColumn>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col lg:flex-row items-start gap-10">
+          <NavButton text="🏠Home" href="/" />
+          <ProjectColumn title="Semester 1">
+            <ProjectLink
+              name="Euro-Dollar converter"
+              href="/projects/eurodollarconverter"
+            />
+            <ProjectLink
+              name="Shop Register System"
+              href="/projects/shop-register-system"
+            />
+            <ProjectLink name="StudyCluster" href="/projects/studycluster" />
+          </ProjectColumn>
+          <ProjectColumn title="Semester 2">
+            <ProjectLink name="Circus Train" href="/projects/circustrain" />
+            <ProjectLink
+              name="Bolus Calculator"
+              href="/projects/bolus-calculator"
+            />
+            <ProjectLink name="Dream Journal" href="/projects/dream-journal" />
+          </ProjectColumn>
+          <ProjectColumn title="Semester 3">
+            <ProjectLink name="Ninja Game" href="/projects/ninja-game" />
+            <ProjectLink name="Bimpi" href="/projects/bimpi" />
+          </ProjectColumn>
+        </div>
       </div>
     </>
   );
