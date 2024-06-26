1687af0b2d7ae89f2fe25ad6ffab449a1025cff7
Teun <74899257+TeunSpithoven@users.noreply.github.com>
2024-06-26 15:37:41 +0200

    Update page.tsx

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -18,8 +18,8 @@ export default function ProjectPage({ params }: { params: Params }) {
     <div className="flex h-screen w-screen px-4 md:px-20 lg:px-40 pt-10 md:pt-0">
       <NavButton text="👈Projects" href="/projects" />
       <div className="flex w-full flex-col gap-4 items-center justify-start pt-4 md:px-20 lg:px-30 xl:px-40 md:pt-0 md:mt-36 h-fit font-bold text-6xl">
-        <h3 className="font-semibold font-sans text-3xl">{title}</h3>
-        <p className="text-xl">{description}</p>
+        <h3 className="font-semibold font-sans text-2xl">{title}</h3>
+        <p className="text-lg">{description}</p>
         {images && (
           <div className="flex gap-4 flex-row items-start flex-wrap w-screen px-10 pb-10 justify-center">
             {images.map((image) => (
